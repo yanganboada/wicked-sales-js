7014fcb5a04e7944d825079c807de7d20064ce21
anniean8908 <anniean8908@gmail.com>
2020-11-04 02:24:47 +0000

    User can view the products for sale - Front End | Yang An Boada

---
+++ client/components/product-list.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    fetch('/api/products')
+      .then(res => {
+        if (res.ok) return res.json();
+        throw new Error('Get product failed with status ' + res.status);
+      })
+      .then(res => this.setState({ products: res }))
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <main className="row justify-content-center m-2">
+          {
+            this.state.products.map((product, index) => {
+              return <ProductListItem item={product} key={index} />;
+            })
+          }
+        </main>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
