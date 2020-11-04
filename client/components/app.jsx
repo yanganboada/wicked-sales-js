a7dfcb253c4adf05c8a190db71a1a42d2aa146b4
anniean8908 <anniean8908@gmail.com>
2020-11-04 23:46:44 +0000

    User can view the details of a product - Front End | Yang An Boada

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
+import ProductDetails from './product-details';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+    this.setView = this.setView.bind(this);
+  }
+
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
 
   render() {
-    return (
-      <div>
+    return this.state.view.name === 'catalog'
+      ? <div>
         <Header />
-        <ProductList />
+        <ProductList toggleView={this.setView}/>
       </div>
-    );
+      : <div>
+        <Header />
+        <ProductDetails params={this.state.view.params} toggleView={this.setView}/>
+      </div>;
   }
 }
 
---
+++ client/components/product-details.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class ProductDtails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+    this.handleBackClick = this.handleBackClick.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(`/api/products/${this.props.params.productId}`)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ product: res });
+      })
+      .catch(err => console.error(err));
+  }
+
+  handleBackClick() {
+    this.props.toggleView('catalog', null);
+  }
+
+  render() {
+    return this.state.product
+
+      ? <div className='container'>
+        <div className='row card m-4 p-4'>
+          <div className="back-arrow w-100 m-2 cursor text-muted" onClick={this.handleBackClick} > {<i className="fas fa-arrow-left"></i>} Back to catalog</div>
+          <div className="row justify-content-center m-2 p-2">
+            <img className="col-5 card-img-left" src={this.state.product.image} alt={this.state.product.name} />
+            <div className="col-7">
+              <h4 className="card-title">{this.state.product.name}</h4>
+              <h6 className="card-subtitle mb-2 text-muted">{`$${this.state.product.price}.00`}</h6>
+              <p className="card-text">{this.state.product.shortDescription}</p>
+            </div>
+          </div>
+          <div className="card-text">{this.state.product.longDescription}</div>
+        </div>
+      </div>
+      : null;
+  }
+
+}
+
+export default ProductDtails;
